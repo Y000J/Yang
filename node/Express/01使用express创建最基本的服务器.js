4ea03e84c8196c34a68247230287e938fb687b02
YJ <534048644@qq.com>
2022-06-06 21:24:00 +0800

    express

--- node/Express/01使用express创建最基本的服务器.js
+++ node/Express/01使用express创建最基本的服务器.js
@@ -2,6 +2,15 @@
 const express = require('express');
 // 创建web服务器
 const app = express()
+// 监听客户端get
+app.get('/', function (req, res) {
+    console.log(req.query, res.params);
+    // 响应内容
+    res.send('请求成功get789' + req.query)
+});
+app.post('/', function (req, res) {
+    res.send('请求成功')
+})
 // 启动web服务器
 app.listen(80, () => {
     console.log('express server running at http://127.0.0.1');
